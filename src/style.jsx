2ccc7f7d9dd8e3035a2d6ed45fd79824ee0e670a
Iago Torres <iagoportal10@gmail.com>
2024-11-29 15:39:36 -0300

    alteração

--- src/style.jsx
+++ src/style.jsx
@@ -15,4 +15,26 @@ export const Copo = styled.img`
 position: relative;
 top: 400px;
 left: 100px;
+`;
+
+export const Button = styled.button`
+background-color: #037143;
+  border: none;
+  color: white;
+  padding: 10px 20px;
+  border-radius: 30px;
+  cursor: pointer;
+`;
+
+export const Span = styled.span`
+background-color: #037143;
+font-size: 56px;
+`;
+
+export const Text = styled.p`
+width: 60%;
+
+`
+export const Título = styled.h2`
+font-size: 40px;
 `
